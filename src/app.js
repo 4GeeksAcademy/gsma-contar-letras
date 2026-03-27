257a040d88c012ceae7978cfebfdd949c50f43c0
yenobis1991-collab <yenobis1991@gmail.com>
2026-03-27 20:45:52 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,42 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+
+  for (let index = 0; index < pronoun.length; index++) {
+  for (let second = 0; second < adj.length; second++) {
+  for (let third = 0; third < noun.length; third++) {
+    
+    const dominio = pronoun[index] + adj[second] + noun[third] + ".com";
+    
+    console.log(dominio);
+     
+    document.body.innerHTML += dominio + "<br>";
+    
+    
+  } 
+    
+  }
+  }
+
+
 };
+
+
+// forEach
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+
+  pronoun.forEach(index => {
+   adj.forEach(second => {
+    noun.forEach(third => {
+      console.log(index + second + third + ".com");
+      
+  
+  })
+  })
+  })
+
